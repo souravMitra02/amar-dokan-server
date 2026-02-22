8b6da581a6e940a752bdb62a3edc522c1575eaf2
Analytics Dashboard <developer@analytics-dashboard.local>
2026-02-22 22:08:26 +0600

    add login functionality to authController and update auth routes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/Users');
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const register = async (req, res) => {
     try {
         const { name, email, password, number } = req.body;
@@ -28,4 +28,39 @@ const register = async (req, res) => {
     }
 };
 
-module.exports = register;
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "User not found!" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Password wrong" });
+        }
+        const token = jwt.sign(
+            { id: user._id, role: user.role }, 
+            process.env.JWT_SECRET, 
+            { expiresIn: '1d' }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: "Server-error", error: error.message });
+    }
+};
+
+module.exports = {register, login};
