a877f5331d181f6b87c5af6899620edb03b4712e
Analytics Dashboard <developer@analytics-dashboard.local>
2026-02-22 21:29:32 +0600

    add auth routes for user registration

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-
+const authRoutes = require('./routes/authRoutes');
 dotenv.config();
 
 connectDB();
@@ -12,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json()); 
 
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('AmarDokan Server is Running smoothly!');
